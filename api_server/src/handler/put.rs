3ba3b7b8b74169de530ca0891e13fa4fd37cc5a6
Thomas <thomas.fractalbits@gmail.com>
2024-10-23 14:38:09 -0700

    api_server: change put_object ok return type

--- api_server/src/handler/put.rs
+++ api_server/src/handler/put.rs
@@ -6,12 +6,12 @@ use axum::{
 };
 use nss_rpc_client::rpc_client::RpcClient;
 
-pub async fn put_object(request: Request, key: String, rpc_client: &RpcClient) -> Result<String> {
+pub async fn put_object(request: Request, key: String, rpc_client: &RpcClient) -> Result<()> {
     let value: String = request.extract().await?;
     let _resp = nss_rpc_client::nss_put_inode(rpc_client, key, value)
         .await
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response())?;
     // serde_json::to_string_pretty(&resp.result)
     // .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into());
-    Ok("".into())
+    Ok(())
 }

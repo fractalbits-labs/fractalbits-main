15000c20b5da7470c25ecb02c9055970aec949aa
Thomas <thomas.fractalbits@gmail.com>
2024-11-17 14:29:08 -0800

    api_server: return xml format results for listObjectsV2 api
    empty for now

--- api_server/src/handler/list.rs
+++ api_server/src/handler/list.rs
@@ -1,10 +1,12 @@
+use crate::response_xml::Xml;
 use axum::{
     extract::{Query, Request},
     http::StatusCode,
-    response, RequestExt,
+    response::{self, IntoResponse, Response},
+    RequestExt,
 };
 use rpc_client_nss::RpcClientNss;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
@@ -20,13 +22,63 @@ struct ListObjectsV2Options {
     start_after: Option<String>,
 }
 
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct ListBucketResult {
+    is_truncated: bool,
+    contents: Contents,
+    name: String,
+    prefix: String,
+    delimiter: String,
+    max_keys: usize,
+    common_prefixes: CommonPrefixes,
+    encoding_type: String,
+    key_count: usize,
+    continuation_token: String,
+    next_continuation_token: String,
+    start_after: String,
+}
+
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct Contents {
+    checksum_algorithm: String,
+    etag: String,
+    key: String,
+    last_modified: String, // timestamp
+    owner: Owner,
+    restore_status: RestoreStatus,
+    size: usize,
+    storage_class: String,
+}
+
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct Owner {
+    display_name: String,
+    id: String,
+}
+
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct RestoreStatus {
+    is_restore_in_progress: bool,
+    restore_expiry_date: String, // timestamp
+}
+
+#[derive(Default, Debug, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct CommonPrefixes {
+    prefix: String,
+}
+
 pub async fn list_objects_v2(
     mut request: Request,
     _rpc_client_nss: &RpcClientNss,
-) -> response::Result<()> {
+) -> response::Result<Response> {
     let Query(opts): Query<ListObjectsV2Options> = request.extract_parts().await?;
     if opts.list_type != Some("2".into()) {
         return Err((StatusCode::BAD_REQUEST, "list-type wrong").into());
     }
-    todo!()
+    Ok(Xml(ListBucketResult::default()).into_response())
 }

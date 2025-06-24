7d5ecfeab9b3d253c85e089d0e7545ddb2d1c0bb
Thomas <thomas.fractalbits@gmail.com>
2025-06-24 16:09:59 -0700

    refactor: move bb8 pool based conn_manager into common rpc client directory

--- api_server/rpc_client/nss/src/lib.rs
+++ api_server/rpc_client/nss/src/lib.rs
@@ -2,7 +2,7 @@ pub mod codec;
 pub mod message;
 pub mod rpc;
 
-mod rpc_client;
+pub(crate) mod rpc_client;
 pub use rpc_client::RpcClient as RpcClientNss;
 pub use rpc_client::RpcError as RpcErrorNss;
 

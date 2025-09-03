7013332e32ffddce1b9f3eb4a47e59d7fe50a77d
Thomas <thomas.fractalbits@gmail.com>
2025-09-03 16:23:09 -0700

    refactor(api_server): clean up for signature errors

--- api_server/src/handler/common/signature/error.rs
+++ api_server/src/handler/common/signature/error.rs
@@ -5,14 +5,10 @@ use thiserror::Error;
 /// Errors of this crate
 #[derive(Debug, Error)]
 pub enum SignatureError {
-    // #[error(display = "{}", _0)]
-    // /// Error from common error
-    // Common(CommonError),
     /// Authorization Header Malformed
     #[error("Authorization header malformed, unexpected scope: {0}")]
     AuthorizationHeaderMalformed(String),
 
-    // Category: bad request
     /// The request contained an invalid UTF-8 sequence in its path or in other parameters
     #[error("Invalid UTF-8: {0}")]
     InvalidUtf8Str(#[from] std::str::Utf8Error),
@@ -42,19 +38,6 @@ pub enum SignatureError {
 
     #[error("Other: {0}")]
     Other(String),
-}
-
-impl From<Box<dyn std::error::Error + Send + Sync>> for SignatureError {
-    fn from(err: Box<dyn std::error::Error + Send + Sync>) -> Self {
-        SignatureError::Other(err.to_string())
-    }
-}
-
-impl From<std::convert::Infallible> for SignatureError {
-    fn from(_: std::convert::Infallible) -> Self {
-        // Infallible can never actually be constructed, so this is unreachable
-        unreachable!("Infallible can never be constructed")
-    }
 }
 
 impl From<aws_signature::SignatureError> for SignatureError {
